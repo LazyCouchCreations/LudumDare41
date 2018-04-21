c5b0e6fd429d56abe9855d56bbe384416c567f6f
Epiin <george.seerden@gmail.com>
2018-04-21 03:42:33 -0400

    added core mechanics

---
+++ WubbaLubbaDubDub/Assets/Scripts/dropper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dropper : MonoBehaviour {
+
+    public float velocity = 5f;
+    public Rigidbody rb;
+    public Transform dropSpot;
+    public GameObject[] shapes;
+
+	// Use this for initialization
+	void Start () {
+       rb.velocity = new Vector3(velocity,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SpawnShape();
+        }
+	}
+
+    private void SpawnShape()
+    {
+        GameObject shape = shapes[Random.Range(0, shapes.Length)];
+        Instantiate(shape, dropSpot.position, Quaternion.identity);
+    }
+}
---
+++ WubbaLubbaDubDub/Assets/Scripts/shape.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class shape : MonoBehaviour {
+
+    public List<GameObject> currentCollisions = new List<GameObject>();
+    public Material mainMaterial;
+    public Material highlightMaterial;
+    public Renderer rend;
+    LineRenderer lineRenderer;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.widthMultiplier = .1f;
+        lineRenderer.enabled = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == tag)
+        {
+            currentCollisions.Add(collision.gameObject);
+        }
+
+        if(currentCollisions.Count == 2)
+        {
+            foreach(GameObject gObject in currentCollisions)
+            {
+                Destroy(gObject);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == tag)
+        {
+            currentCollisions.Remove(collision.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hitInfo = new RaycastHit();
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo) && hitInfo.collider.gameObject == gameObject)
+            {
+                rend.material = highlightMaterial;
+                lineRenderer.SetPosition(0, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -5));
+                lineRenderer.enabled = true;
+            }
+            else
+            {
+                rend.material = mainMaterial;
+            }
+        }
+
+        if (Input.GetButton("Fire1") && lineRenderer.enabled == true)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            lineRenderer.SetPosition(1, new Vector3(mousePos.x, mousePos.y, -5));
+        }
+
+        if(Input.GetButtonUp("Fire1") && lineRenderer.enabled == true)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 line = lineRenderer.GetPosition(0) - lineRenderer.GetPosition(1);
+            rb.AddForce(line * -1, ForceMode.Impulse);
+            lineRenderer.enabled = false;
+        }
+    }
+}
