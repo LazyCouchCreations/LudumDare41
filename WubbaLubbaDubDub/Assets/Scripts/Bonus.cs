ab1c7e584dbdae20c507fb90527c6490ed9fa2ea
Epiin <george.seerden@gmail.com>
2018-04-22 19:44:49 -0400

    fixed bonus bug

--- WubbaLubbaDubDub/Assets/Scripts/Bonus.cs
+++ WubbaLubbaDubDub/Assets/Scripts/Bonus.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Bonus : MonoBehaviour {
@@ -19,6 +17,10 @@ public class Bonus : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         bonus = (int)((powerSliderImage.fillAmount * 100f) / 25);
+        if (bonus == sprites.Length)
+        {
+            bonus = sprites.Length - 1;
+        }
         myImage.sprite = sprites[bonus];
 	}
 }
