4f7c7f013912f5dd37f491e7f65d37ab2b59f9bc
Epiin <george.seerden@gmail.com>
2018-04-22 20:53:49 -0400

    final publish

---
+++ WubbaLubbaDubDub/Assets/music.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class music : MonoBehaviour {
+
+    public AudioSource themeMusic;
+    public Slider me;
+
+	// Use this for initialization
+	void Start () {
+        me = GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        themeMusic.volume = me.value;
+    }
+}
